a58edec923f6afdf2fe686fd139c4878458af877
nickwillson <132080833+nickwillson@users.noreply.github.com>
2024-06-12 11:57:00 +0600

    Update install_requirements.sh
    new_one

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Check if python3 is installed
-if ! command -v python3 &>/dev/null; then
-    sudo apt-get update
-    sudo apt-get install -y python3
-fi
+# List of dependencies
+dependencies=("python3" "python3-pip" "python3-cryptography" "python3-openssl")
 
-# Check if pip3 is installed
-if ! command -v pip3 &>/dev/null; then
-    sudo apt-get install -y python3-pip
-fi
-
-# Install required Python packages
-sudo pip3 install -r requirements.txt
+# Check if dependencies are installed
+for dep in "${dependencies[@]}"; do
+    if ! dpkg -s "$dep" >/dev/null 2>&1; then
+        echo "Installing $dep"
+        sudo apt-get install -y "$dep"
+    else
+        echo "$dep is already installed"
+    fi
+done
